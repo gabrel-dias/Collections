f52acfa6b0a836daaae72057e88c4c0285e7dba9
Dias9K <gabrielmdais557@gmail.com>
2024-01-05 00:12:33 -0300

    Agenda de Contatos (Set)

--- src/MeusExercícios/set/pesquisa/Contato.java
+++ src/MeusExercícios/set/pesquisa/Contato.java
@@ -1,5 +1,7 @@
 package MeusExercícios.set.pesquisa;
 
+import java.util.Objects;
+
 public class Contato {
     private String nome;
     private int numeroDeTelefone;
@@ -17,9 +19,61 @@ public class Contato {
         return numeroDeTelefone;
     }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNumeroDeTelefone(int numeroDeTelefone) {
+        this.numeroDeTelefone = numeroDeTelefone;
+    }
+
     @Override
     public String toString() {
         return "Contato [nome=" + nome + ", numeroDeTelefone=" + numeroDeTelefone + "]";
     }
 
+    public Contato(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Contato other = (Contato) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+//     @Override
+//     public int hashCode() {
+//         return Objects.hash(getNome());
+//     }
+
+
+//     @Override
+//   public boolean equals(Object o) {
+//     if (this == o) return true;
+//     if (!(o instanceof Contato contato)) return false;
+//     return Objects.equals(getNome(), contato.getNome());
+//   }
+
+
+   
 }
