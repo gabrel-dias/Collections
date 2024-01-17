5c0fc99dfa8a716047514c2bf2eed3995de40858
Dias9K <gabrielmdais557@gmail.com>
2024-01-16 22:57:35 -0300

    exemploSet2 1 (métodos)

--- src/CursoAntigo/ExemploSet2.java
+++ src/CursoAntigo/ExemploSet2.java
@@ -4,22 +4,24 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ExemploSet2 {
-  public static void main(String[] args) {
-    System.out.println("Ordem de séries aleatória...");
-    Set<Serie> seriesHashSet = new HashSet<>(){{
-        add(new Serie("got", "fantasia", 60));
-        add(new Serie("dark", "drama", 60));
-        add(new Serie("friends", "comédia", 24));
-    }};
-    System.out.println(seriesHashSet);
-  }  
+    public static void main(String[] args) {
+        System.out.println("Ordem de séries aleatória...");
+        Set<Serie> seriesHashSet = new HashSet<>() {
+            {
+                add(new Serie("got", "fantasia", 60));
+                add(new Serie("dark", "drama", 60));
+                add(new Serie("friends", "comédia", 24));
+            }
+        };
+        System.out.println(seriesHashSet);
+    }
 }
 
 class Serie {
     private String nome;
     private String genero;
     private int tempoEp;
-    
+
     public Serie(String nome, String genero, int tempoEp) {
         this.nome = nome;
         this.genero = genero;
@@ -42,7 +44,41 @@ class Serie {
     public String toString() {
         return "[nome=" + nome + ", genero=" + genero + ", tempoEp=" + tempoEp + "]";
     }
-    
 
+    // a IDE criou e não sei nada disso ainda
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((genero == null) ? 0 : genero.hashCode());
+        result = prime * result + tempoEp;
+        return result;
+    }
+
+    // também não sei
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Serie other = (Serie) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (genero == null) {
+            if (other.genero != null)
+                return false;
+        } else if (!genero.equals(other.genero))
+            return false;
+        if (tempoEp != other.tempoEp)
+            return false;
+        return true;
+    }
 
 }
