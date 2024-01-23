59ec3510ace7c3af0600630424c1220fa0437c4f
Dias9K <gabrielmdais557@gmail.com>
2024-01-23 02:36:02 -0300

    desisto por hoje

--- src/cursoAntigo/exercíciosPropostos/SetExercicioProposto02.java
+++ src/cursoAntigo/exercíciosPropostos/SetExercicioProposto02.java
@@ -10,11 +10,11 @@ public class SetExercicioProposto02 {
      * Crie uma classe LinguagemFavorita que possua os atributos nome, anoDeCriacao
      * e ide. Em seguida, crie um conjunto com 3 linguagens e faça um programa que
      * ordene esse conjunto por:
-     * Ordem de Inserção (LinkedHashSet)
-     * Ordem Natural (nome) (LinkedHashSet e o Comparable)
-     * IDE
-     * Ano de criação e nome
-     * Nome, ano de criacao e IDE
+     * 1. Ordem de Inserção (LinkedHashSet)
+     * 2. Ordem Natural (nome) (LinkedHashSet e o Comparable)
+     * 3. IDE
+     * 4. Ano de criação e nome
+     * 5. Nome, ano de criacao e IDE
      * Ao final, exiba as linguagens no console, um abaixo da outra.
      */
 
