81f5dd61b37ca3fd3ad77471833c69a67ab82333
Dias9K <gabrielmdais557@gmail.com>
2024-02-11 11:42:25 -0300

    agendaStream

--- src/cursoantigo/stream/Agenda.java
+++ src/cursoantigo/stream/Agenda.java
@@ -1,34 +1,45 @@
 package cursoantigo.stream;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Agenda {
     public static void main(String[] args) {
-        Map<Integer, Contato> agendMap = new HashMap<>(){{
-            put(1, new Contato("Simba", 2222));
-            put(4, new Contato("Cami", 5555));
-            put(3, new Contato("Jon", 1111));
-        }};
-    }
-}
+        System.out.println("\t--Ordem aleatória--\t");
+        Map<Integer, Contato> agenda = new HashMap<>() {
+            {
+                put(1, new Contato("Simba", 2222));
+                put(4, new Contato("Cami", 5555));
+                put(3, new Contato("Jon", 1111));
+            }
+        };
+        System.out.println(agenda);
+        for (Map.Entry<Integer, Contato> entry : agenda.entrySet()) { // entrySet sendo utilizado para trabalhar com os
+            // elementos do dicionário de forma separada
+            System.out.println(entry.getValue().getNome() + " - " + entry.getValue().getNumero());
+        }
 
-class Contato {
-    private String nome;
-    private int numero;
-    public String getNome() {
-        return nome;
+        System.out.println("\t--Ordem de inserção--\t");
+        Map<Integer, Contato> agenda2 = new LinkedHashMap<>() {
+            {
+                put(3, new Contato("Jon", 1111));
+                put(4, new Contato("Cami", 5555));
+                put(1, new Contato("Simba", 2222));
+                put(2, new Contato("Gabri", 6666));
+            }
+        };
+        System.out.println(agenda2);
+        for (Map.Entry<Integer, Contato> entry : agenda2.entrySet()) {
+            System.out.println(entry.getValue().getNome() + " - " + entry.getValue().getNumero());
+        }
+
+        System.out.println("\t--Ordem de chave--\t");
+        Map<Integer, Contato> agenda3 = new TreeMap<>(agenda2);
+        System.out.println(agenda3);
+        for (Map.Entry<Integer, Contato> entry : agenda3.entrySet()) {
+            System.out.println(entry.getValue().getNome() + " - " + entry.getValue().getNumero());
+        }
     }
-    public int getNumero() {
-        return numero;
-    }
-    public Contato(String nome, int numero) {
-        this.nome = nome;
-        this.numero = numero;
-    }
-    @Override
-    public String toString() {
-        return "Contato [nome=" + nome + ", numero=" + numero + "]";
-    }
-    
 }
