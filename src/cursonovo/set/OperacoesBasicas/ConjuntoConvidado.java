8904815fd62dc1240968f070176a3f59e921356e
Gabriel Dias <gabrielmdais557@gmail.com>
2024-06-05 11:16:29 -0300

    isso tá me dando um nó mas vamos lá

--- src/cursonovo/set/OperacoesBasicas/ConjuntoConvidado.java
+++ src/cursonovo/set/OperacoesBasicas/ConjuntoConvidado.java
@@ -4,17 +4,36 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ConjuntoConvidado {
+    // atributo
     private Set<Convidado> convidadoSet;
 
-    public ConjuntoConvidado(Set<Convidado> convidadoSet) {
+    // construtor
+    public ConjuntoConvidado() {
         this.convidadoSet = new HashSet<>();
     }
-
+    // métodos
     public void adicionarConvidados(String nome, int codigoConvite){
         convidadoSet.add(new Convidado(nome, codigoConvite));
         // o método add requer um elemento como atributo; por isso, é passado o construtor da classe convidado que precisa do nome e codigo
     }
-    public void removerConvidadoPorCodigo(String codigoConvite){
+
+    public void removerConvidadoPorCodigo(int codigoConvite){
         // o código do convite é a chave estrangeira do convidado
+        Convidado convidadoARemover = null;
+        for(Convidado c : convidadoSet){
+            if (c.getCodigoConvite() == codigoConvite){
+                convidadoARemover = c;
+                break;
+            }
+        }
+    convidadoSet.remove(convidadoARemover);
+    }
+
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
     }
 }
